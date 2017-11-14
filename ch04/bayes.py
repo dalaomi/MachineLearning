7b7e6f3a34f7a859d34217cd64a08e00bfad0b42
xiaolianghao <xiaolianghao@creditease.cn>
2017-11-14 14:42:12 +0800

    贝叶斯-垃圾邮件过滤

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -117,6 +117,76 @@ def spamTest():
         else:
             print 'w'
 
+def calcMostFreq(vocabList, fullText):
+    import operator
+    freDict = {}
+    for token in vocabList:
+        freDict[token] = fullText.count(token)
+    sortedFreq = sorted(freDict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedFreq[:30]
+
+def localWords(feed1, feed0):
+    import feedparser
+    docList = []
+    classList = []
+    fullText = []
+    minLen = min(len(feed1['entries']), len(feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    top30Words = calcMostFreq(vocabList,fullText)
+    for pairW in top30Words:
+        if pairW[0] in vocabList:
+            vocabList.remove(pairW[0])
+    trainingSet = range(2*minLen)
+    testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWord2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for i in range(20):
+        if classifyNB(setOfWord2Vec(vocabList, docList[testSet[i]]), p0V, p1V, pAb) == classList[testSet[i]]:
+            errorCount += 0
+        else:
+            errorCount += 1
+    print 'error count is ', float(errorCount)/len(testSet)
+    return vocabList,p0V, p1V
+
+def getTopWords(ny, sf):
+    import operator
+    topNY=[]
+    topSF=[]
+    vocabList, p0V, p1V = localWords(ny, sf)
+    for i in range(len(p0V)):
+        if p0V[i] > -6.0:
+            topSF.append((vocabList[i], p0V[i]))
+        if p1V[i] > -6.0:
+            topNY.append((vocabList[i], p1V[i]))
+
+    sortedSF = sorted(topSF, key=lambda pair: pair[1], reverse=True)
+    print 'SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF'
+    for item in sortedSF:
+        print item[0]
+    sortedNY = sorted(topNY, key=lambda pair: pair[1], reverse=True)
+    print 'NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY'
+    for item in sortedNY:
+        print item[0]
+
+
 
 
 
--- ch04/test.py
+++ ch04/test.py
@@ -32,3 +32,13 @@ print bayes.textParse('This is a book ! ')
 
 
 bayes.spamTest()
+
+
+import feedparser
+ny = feedparser.parse('http://newyork.craigslist.org/stp/index.rss')
+sf = feedparser.parse('http://sfbay.craigslist.org/stp/index.rss')
+
+vocabList,pSF,pNY=bayes.localWords(ny,sf)
+
+
+bayes.getTopWords(ny, sf)
